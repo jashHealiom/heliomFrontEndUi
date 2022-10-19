95a64c43250f013b8f162e6423152192d39565fc
jashHealiom <jash@healiom.com>
2022-10-19 11:32:54 -0400

    5 star component

--- src/components/he_StarRating.js
+++ src/components/he_StarRating.js
@@ -1,19 +1,24 @@
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
-import React from 'react'
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import React from 'react';
 
-const StarRating = (props) => {
-    const { style } = props
-    return (
-        <View style={[{ width: '100%' }, style]}>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-            </View>
-        </View>
-    )
-}
+const StarRating = props => {
+  const {style, ratedIndex} = props;
+  return (
+    <View style={[{width: '100%'}, style]}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+      </View>
+    </View>
+  );
+};
 
 export default StarRating;
